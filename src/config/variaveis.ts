498f80689c218cff0a0aa13a15205905912a28d0
felipe <felipe.mschultz@hotmail.com>
2019-11-09 22:14:13 -0300

    Troca da variável de ambiente

--- src/config/variaveis.ts
+++ src/config/variaveis.ts
@@ -2,7 +2,7 @@ const variables = {
   api: {
     port: process.env.PORT || 3001
   },
-  banco: process.env.connection || 'mongodb://localhost/tcc-dani',
+  banco: process.env.CONNECTION || 'mongodb://localhost/tcc-dani',
   // banco: process.env.connection || 'mongodb+srv://admin:0000@cluster0-d69u7.mongodb.net/dani?retryWrites=true&w=majority',
   seguranca: {
     secretKey: 'eusoqueromeformarlogo'
